715566879d91b58a52276aae2b806e2be1b41a0d
Casper Welzel Andersen <43357585+CasperWA@users.noreply.github.com>
2022-12-06 15:57:01 +0100

    Test updated CD - release workflow (#87)
    Update ci_tests workflow and docs.
    Remove unused files and suggest changes.

--- .github/utils/update_tags.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-echo "### Move/update v<MAJOR> tag ###"
-MAJOR_VERSION=$( echo ${GITHUB_REF#refs/tags/v} | cut -d "." -f 1 )
-TAG_MSG=.github/utils/major_version_tag_msg.txt
-sed -i "s|MAJOR|${MAJOR_VERSION}|g" "${TAG_MSG}"
-
-git tag -af -F "${TAG_MSG}" v${MAJOR_VERSION}
